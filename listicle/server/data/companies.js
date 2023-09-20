f9b79f43cd5360354cd295499e22415a72999f5b
Sanskar Lamsal <lamsal9677@yahoo.com>
2023-09-19 22:09:35 -0400

    listicle init

---
+++ listicle/server/data/companies.js
@@ -0,0 +1,123 @@
+const companies = [
+      {
+        "name": "Airbnb",
+        "headquarters": "San Francisco, California",
+        "revenue": "$4.8 billion",
+        "y_combinator_season": "2009 Summer"
+      },
+      {
+        "name": "DoorDash",
+        "headquarters": "San Francisco, California",
+        "revenue": "$885 million",
+        "y_combinator_season": "S10"
+      },
+      {
+        "name": "Stripe",
+        "headquarters": "San Francisco, California",
+        "revenue": "$95 billion",
+        "y_combinator_season": "S10"
+      },
+      {
+        "name": "Cruise",
+        "headquarters": "San Francisco, California",
+        "revenue": "$1 billion+ (Acquired by GM)",
+        "y_combinator_season": "W14"
+      },
+      {
+        "name": "Coinbase",
+        "headquarters": "San Francisco, California",
+        "revenue": "$1.3 billion",
+        "y_combinator_season": "S12"
+      },
+      {
+        "name": "Rappi",
+        "headquarters": "Bogotá, Colombia",
+        "revenue": "$1 billion",
+        "y_combinator_season": "W16"
+      },
+      {
+        "name": "Ginkgo Bioworks",
+        "headquarters": "Boston, Massachusetts",
+        "revenue": "$15 billion",
+        "y_combinator_season": "S14"
+      },
+      {
+        "name": "Doorport",
+        "headquarters": "San Francisco, California",
+        "revenue": "Undisclosed",
+        "y_combinator_season": "W21"
+      },
+      {
+        "name": "CoinList",
+        "headquarters": "San Francisco, California",
+        "revenue": "Undisclosed",
+        "y_combinator_season": "S17"
+      },
+      {
+        "name": "Atrium",
+        "headquarters": "San Francisco, California",
+        "revenue": "Undisclosed",
+        "y_combinator_season": "W16"
+      },
+      {
+        "name": "Boom Supersonic",
+        "headquarters": "Denver, Colorado",
+        "revenue": "Undisclosed",
+        "y_combinator_season": "W18"
+      },
+      {
+        "name": "Instacart",
+        "headquarters": "San Francisco, California",
+        "revenue": "$39 billion",
+        "y_combinator_season": "S12"
+      },
+      {
+        "name": "Brex",
+        "headquarters": "San Francisco, California",
+        "revenue": "$7.4 billion",
+        "y_combinator_season": "S17"
+      },
+      {
+        "name": "Reddit",
+        "headquarters": "San Francisco, California",
+        "revenue": "$10 billion",
+        "y_combinator_season": "S05"
+      },
+      {
+        "name": "Zapier",
+        "headquarters": "Sunnyvale, California",
+        "revenue": "Acquired by Intuit for $1.2 billion",
+        "y_combinator_season": "S12"
+      },
+      {
+        "name": "Craigslist",
+        "headquarters": "San Francisco, California",
+        "revenue": "Undisclosed",
+        "y_combinator_season": "S02"
+      },
+      {
+        "name": "Grove Collaborative",
+        "headquarters": "San Francisco, California",
+        "revenue": "$104 million",
+        "y_combinator_season": "S15"
+      },
+      {
+        "name": "Faire",
+        "headquarters": "San Francisco, California",
+        "revenue": "$7 billion",
+        "y_combinator_season": "W17"
+      },
+      {
+        "name": "Counsyl",
+        "headquarters": "South San Francisco, California",
+        "revenue": "$375 million+ (Acquired by Myriad Genetics)",
+        "y_combinator_season": "W09"
+      },
+      {
+        "name": "Ginkgo Bioworks",
+        "headquarters": "Boston, Massachusetts",
+        "revenue": "$15 billion",
+        "y_combinator_season": "S14"
+      }
+    ];
+export default companies;
